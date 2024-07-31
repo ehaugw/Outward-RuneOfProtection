da1ff5f11a6ee63240b7a783995a260e3dc428c5
Eivind <ehaugw@gmail.com>
2024-07-31 10:11:53 +0200

    initial commit

---
+++ RuneOfProtection.cs
@@ -0,0 +1,51 @@
+﻿namespace RuneOfProtection
+{
+    using BepInEx;
+    using HarmonyLib;
+    using InstanceIDs;
+    using SideLoader;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using UnityEngine;
+
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class RuneOfProtection : BaseUnityPlugin
+    {
+        public const string GUID = "com.ehaugw.runeofprotection";
+        public const string VERSION = "1.0.0";
+        public const string NAME = "Rune of Protection";
+        public static string ModFolderName = Directory.GetParent(typeof(RuneOfProtection).Assembly.Location).Name.ToString();
+
+        internal void Awake()
+        {
+            var harmony = new Harmony(GUID);
+            harmony.PatchAll();
+
+            SL.OnPacksLoaded += OnPackLoaded;
+            SL.OnSceneLoaded += OnSceneLoaded;
+        }
+        private void OnPackLoaded()
+        {
+            RuneOfProtectionItem.MakeItem();
+            RuneOfProtectionItem.MakeRecipe();
+        }
+
+        private void OnSceneLoaded()
+        {
+            foreach (GameObject obj in Resources.FindObjectsOfTypeAll<GameObject>().Where(x => x.name == "UNPC_LaineAberforthA" || x.name == "HumanSNPC_CounterAlchemist"))
+            {
+                if (obj.GetComponentsInChildren<GuaranteedDrop>()?.FirstOrDefault(table => table.ItemGenatorName == "Recipes") is GuaranteedDrop recipes)
+                {
+                    if (At.GetField<GuaranteedDrop>(recipes, "m_itemDrops") is List<BasicItemDrop> drops)
+                    {
+                        foreach (Item item in new Item[] { ResourcesPrefabManager.Instance.GetItemPrefab(IDs.runeOfProtectionRecipeID) })
+                        {
+                            drops.Add(new BasicItemDrop() { ItemRef = item, MaxDropCount = 1, MinDropCount = 1 });
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ RuneOfProtectionItem.cs
@@ -0,0 +1,86 @@
+﻿
+namespace RuneOfProtection
+{
+    using System.Collections.Generic;
+    using SideLoader;
+    using InstanceIDs;
+    using TinyHelper;
+
+    public class RuneOfProtectionItem
+    {
+        public const string SubfolderName = "RuneOfProtection";
+        public const string ItemName = "Rune of Protection";
+
+        public static Item MakeItem()
+        {
+            var myitem = new SL_Item()
+            {
+                Name = ItemName,
+                EffectBehaviour = EditBehaviours.Destroy,
+                Target_ItemID = IDs.blessedPotionID,
+                New_ItemID = IDs.runeOfProtectionID,
+                Description = "Grants Runic Protection when used.",
+                
+                CastType = Character.SpellCastType.Brace,
+                CastModifier = Character.SpellCastModifier.Immobilized,
+                CastLocomotionEnabled = false,
+                MobileCastMovementMult = -1,
+                CastSheatheRequired = 1,
+                
+                StatsHolder = new SL_ItemStats()
+                {
+                    RawWeight = 0.5f,
+                    BaseValue = 100,
+                },
+
+                EffectTransforms = new SL_EffectTransform[] {
+                    new SL_EffectTransform() {
+                        TransformName = "Effects",
+                        Effects = new SL_Effect[] {
+                        new SL_AddStatusEffect()
+                            {
+                                StatusEffect = IDs.runicProtectionID
+                            }
+                        }
+                    }
+                },
+
+                SLPackName = RuneOfProtection.ModFolderName,
+                SubfolderName = SubfolderName
+            };
+            myitem.ApplyTemplate();
+            Item item = ResourcesPrefabManager.Instance.GetItemPrefab(myitem.New_ItemID);
+
+            return item;
+        }
+
+        public static Item MakeRecipe()
+        {
+            string newUID = TinyUIDManager.MakeUID(ItemName, RuneOfProtection.GUID, "Recipe");
+            new SL_Recipe()
+            {
+                StationType = Recipe.CraftingType.Alchemy,
+                Results = new List<SL_Recipe.ItemQty>() {
+                    new SL_Recipe.ItemQty() { Quantity = 1, ItemID = IDs.runeOfProtectionID},
+                },
+                Ingredients = new List<SL_Recipe.Ingredient>() {
+                    new SL_Recipe.Ingredient() { Type = RecipeIngredient.ActionTypes.AddSpecificIngredient, Ingredient_ItemID = IDs.palladiumScrapsID},
+                    new SL_Recipe.Ingredient() { Type = RecipeIngredient.ActionTypes.AddSpecificIngredient, Ingredient_ItemID = IDs.crystalPowderID},
+                },
+                UID = newUID,
+            }.ApplyTemplate();
+
+            var myitem = new SL_RecipeItem()
+            {
+                Name = "Alchemy: " + ItemName,
+                Target_ItemID = IDs.arbitraryAlchemyRecipeID,
+                New_ItemID = IDs.runeOfProtectionRecipeID,
+                EffectBehaviour = EditBehaviours.Override,
+                RecipeUID = newUID
+            };
+            myitem.ApplyTemplate();
+            Item item = ResourcesPrefabManager.Instance.GetItemPrefab(myitem.New_ItemID);
+            return item;
+        }
+    }
+}
